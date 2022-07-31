7d1b7a23c7bdfbbd5e67ef5e2107bdfec2dd2858
Ashiq Legi <ashiqlegi@gmail.com>
2022-07-31 14:03:05 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const submitData = {
+    method: "POST",
+    headers :{
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: "Carl Sagan",
+        email: "Carlagan@yahoo.com",
+    }),
+
+}
+
+fetch("http://localhost:3000/users", submitData)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        console.log(object)
+    })
+    .catch(function (error) {
+        console.log(error.message)
+    })
